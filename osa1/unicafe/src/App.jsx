cb8593bb9df8fe026de981aba22615fbb22814c1
Pinja <pinja.salomaki@gmail.com>
2023-12-02 08:49:32 +0200

    1.7 tehty

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -11,15 +11,30 @@ const Button = ({ handleClick, text }) => {
     </button>
   )
 }
-
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [count, setCount] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const rateGood = () => setGood(good + 1)
-  const rateNeutral= () => setNeutral(neutral + 1)
-  const rateBad= () => setBad(bad + 1)
+  const rateGood = () => {
+    console.log("good ", good)
+    setGood(good + 1)
+    setCount(count + 1)
+    setTotal(total + 1)
+  }
+
+  const rateNeutral= () => {
+    setNeutral(neutral + 1)
+    setCount(count + 1)
+  }
+
+  const rateBad= () => {
+    setBad(bad + 1) 
+    setCount(count + 1)
+    setTotal(total - 1)
+  }
 
   return (
     <div>
@@ -32,6 +47,9 @@ const App = () => {
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
+      <Display text="all" value={count} />
+      <Display text="average" value={(total/count)}/>
+      <Display text="positive" value={good/count*100 + '%'}/>
     </div>
   )
 }
