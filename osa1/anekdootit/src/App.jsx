0ca9d3f09156d7d81cc8df32403863fd80b88af5
Pinja <pinja.salomaki@gmail.com>
2023-12-18 16:43:45 +0200

    1.13 tehty

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -10,7 +10,6 @@ const Button = ({ handleClick, text }) => {
   )
 }  
 
-
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -22,15 +21,29 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
+
    
   const [selected, setSelected] = useState(0)
+  const [selected_points, setPoints] = useState(new Array(8).fill(0))
 
-    const getAnecdote = () => () => setSelected(Math.floor(Math.random()*anecdotes.length))
+  const getAnecdote = () => () => setSelected(Math.floor(Math.random()*anecdotes.length))
+
+  const Vote = () => () => {
+    console.log("points before ", selected_points)
+    const copy = [...selected_points]
+    copy[selected] += 1
+    setPoints(copy)
+    console.log("points after ", selected_points)
+  }
 
 
   return (
     <div>
       {anecdotes[selected]}
+      <p>
+        has {selected_points[selected]} votes
+      </p>
+      <Button handleClick={Vote()} text='vote' />
       <Button handleClick={getAnecdote()} text='next anecdote' />
     </div>
   )
