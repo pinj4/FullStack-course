34a3848daf88f8eed75ba6be816ea446078119af
pinj4 <pinja.salomaki@gmail.com>
2024-07-04 13:26:13 +0300

    4.8 tehty

---
+++ osa4/blogilista/tests/bloglist_api_test.js
@@ -0,0 +1,31 @@
+const { test, after, beforeEach} = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+
+test.only('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test.only('there are six blogs', async () => {
+    const response = await api.get('/api/blogs')
+  
+    assert.strictEqual(response.body.length, 6)
+  })
+
+after(async () => {
+  await mongoose.connection.close()
+})
