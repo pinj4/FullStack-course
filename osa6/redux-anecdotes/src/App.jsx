ed085d4672ff9b4bfdf7714cc0efe45262b87be2
pinj4 <pinja.salomaki@gmail.com>
2024-10-04 15:53:27 +0300

    6.5 tehty

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -3,7 +3,7 @@ import { voteAnecdote } from './reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state).sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
   const vote = (id) => {
