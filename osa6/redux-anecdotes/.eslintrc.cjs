e2c3e27074968730a27b9d31d50935a7df0b15b1
pinj4 <pinja.salomaki@gmail.com>
2024-10-04 16:47:36 +0300

    6.8 tehty + lint

--- osa6/redux-anecdotes/.eslintrc.cjs
+++ osa6/redux-anecdotes/.eslintrc.cjs
@@ -16,5 +16,29 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "never"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+        "error", "always"
+    ],
+    "arrow-spacing": [
+        "error", { "before": true, "after": true }
+    ],
+    "no-console": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": 0
   },
 }
---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      {anecdote.content}
+      <div>
+          has {anecdote.votes}
+          <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+    .sort((a, b) => b.votes - a.votes)
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() =>
+            dispatch(voteAnecdote(anecdote.id))
+          }
+        />
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
