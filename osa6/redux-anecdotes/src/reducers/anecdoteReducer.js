23e3eacbc6d9f2304f91069637b5857da7416003
pinj4 <pinja.salomaki@gmail.com>
2024-10-06 11:52:34 +0300

    6.15 tehty

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -9,7 +9,7 @@ import { createSlice } from '@reduxjs/toolkit'
 //   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 // ]
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+// const getId = () => (100000 * Math.random()).toFixed(0)
 
 // const asObject = (anecdote) => {
 //   return {
@@ -28,12 +28,7 @@ const anecdoteSlice = createSlice({
     createAnecdote(state, action) {
       console.log('ACTION: ', action)
       console.log(JSON.parse(JSON.stringify(state)))
-      const content = action.payload
-      state.push({
-        content,
-        id: getId(),
-        votes: 0
-      })
+      state.push(action.payload)
     },
     voteAnecdote(state, action) {
       console.log('ACTION: ', action)
