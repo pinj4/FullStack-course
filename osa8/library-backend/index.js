9ebe0b0e19795ad6d00da5bae2918af84aeb7e8f
pinj4 <pinja.salomaki@gmail.com>
2024-10-23 20:00:49 +0300

    8.4

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -106,7 +106,12 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+        if (!args.author) {
+            return books
+        }
+        return books.filter(b => b.author === args.author)
+    },
     allAuthors: () => authors
   },
 
