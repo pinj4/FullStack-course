20bf19169b86a513821f69fd0c8e1faaba26d203
pinj4 <pinja.salomaki@gmail.com>
2024-10-24 16:20:21 +0300

    refaktorointia

--- osa8/library-frontend/src/components/NewBook.jsx
+++ osa8/library-frontend/src/components/NewBook.jsx
@@ -1,45 +1,6 @@
 import { useState } from 'react'
-import { gql, useMutation } from '@apollo/client'
-
-const CREATE_BOOK = gql `
-  mutation createBook($title: String!, $author: String!,  $published: Int!,
-  $genres: [String]!) {
-    addBook(
-      title: $title,
-      author: $author,
-      published: $published,
-      genres: $genres
-    ) {
-      title
-      author
-      published
-      genres
-    }
-  }
-`
-
-const ALL_BOOKS = gql`
-  query {
-    allBooks {
-      title,
-      author,
-      published
-      id
-    }
-  }
-`
-
-const ALL_AUTHORS = gql`
-  query {
-    allAuthors {
-      name,
-      born,
-      bookCount,
-      id
-    }
-  }
-`
-
+import { useMutation } from '@apollo/client'
+import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
