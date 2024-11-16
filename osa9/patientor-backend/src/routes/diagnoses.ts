39858d0770b6515d7222219473e3455861237cc7
pinj4 <pinja.salomaki@gmail.com>
2024-11-16 09:43:15 +0200

    9.10 tehty

---
+++ osa9/patientor-backend/src/routes/diagnoses.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import diagnosisService from '../services/diagnosisService';
+import { DiagnosisEntry } from '../types';
+
+import { Response } from 'express';
+
+const router = express.Router();
+
+router.get('/', (_req, res: Response<DiagnosisEntry[]>) => {
+  res.send(diagnosisService.getDiagnoses());
+});
+
+router.post('/', (_req, res) => {
+  res.send('Saving a diary!');
+});
+
+export default router;
---
+++ osa9/patientor-backend/src/services/diagnosisService.ts
@@ -0,0 +1,18 @@
+import diagnosisData from '../../data/diagnoses';
+
+import { DiagnosisEntry } from '../types';
+
+const diagnoses: DiagnosisEntry[] = diagnosisData;
+
+const getDiagnoses = (): DiagnosisEntry[] => {
+    return diagnoses;
+};
+
+const addDiagnosis = () => {
+    return null;
+};
+
+export default {
+    getDiagnoses,
+    addDiagnosis
+};
